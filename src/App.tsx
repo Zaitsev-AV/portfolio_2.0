055853c391c25105654df165af7ed062c5e58143
Александр Зайцев <zaitsev.av.12@gmail.com>
2023-11-16 10:28:32 +0300

    feat: added automatic deploy storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
+import {Cards} from "@/components/ui/cards.tsx";
 
 export function App() {
   return (
     <Box>
-      <StyledBtn fontSize='30px' variant='outline' active>Hello</StyledBtn>
-      <StyledBtn color='blue' variant='primary'>Hello</StyledBtn>
+      <Cards/>
+      {/*<StyledBtn fontSize='30px' variant='outline' active>Hello</StyledBtn>*/}
+      {/*<StyledBtn color='blue' variant='primary'>Hello</StyledBtn>*/}
     </Box>
   )
 }
 
-interface SupperButtonProps {
-  fontSize?: string
-  color?: string
-  variant?: 'outline' | 'primary'
-  active?: boolean
-}
+// interface SupperButtonProps {
+//   fontSize?: string
+//   color?: string
+//   variant?: 'outline' | 'primary'
+//   active?: boolean
+// }
 
 const Box = styled.div`
   display: flex;
@@ -24,44 +26,44 @@ const Box = styled.div`
   gap: 20px;
 `
 
-const StyledBtn = styled.button<SupperButtonProps>`
-  border: none;
-  border-radius: 14px;
-  //background-color: brown;
-  background-color: ${props => props.color || '#61942e'};
-  padding: 10px 20px;
-  font-size: ${props => props.fontSize || '25px'};
-  color: #242424;
-
-  ${props => props.variant === 'primary' && css<SupperButtonProps>`
-    background-color: ${props => props.color || '#61942e'};
-    color: #c92c2c;
-    &:hover {
-      background-color: transparent;
-    }
-  `}
-
-
-  ${props => props.variant === 'outline' && css<SupperButtonProps>`
-    color: ${props => props.color || '#61942e'};
-    border: 2px solid ${props => props.color || '#61942e'};
-    background-color: transparent;
-    &:hover {
-      background-color: chartreuse;
-    }
-  `}
-  ${props => props.active && css<SupperButtonProps> `
-    &:hover {
-      box-shadow: 5px 5px 5px 13px rgba(178, 94, 94, 0.25);
-    }
-  `}
-
-  ${props => props.active && css<SupperButtonProps> `
-    &:hover {
-      box-shadow: 5px 5px 5px 13px rgba(178, 94, 94, 0.25);
-    }
-  `}
-`
+// const StyledBtn = styled.button<SupperButtonProps>`
+//   border: none;
+//   border-radius: 14px;
+//   //background-color: brown;
+//   background-color: ${props => props.color || '#61942e'};
+//   padding: 10px 20px;
+//   font-size: ${props => props.fontSize || '25px'};
+//   color: #242424;
+//
+//   ${props => props.variant === 'primary' && css<SupperButtonProps>`
+//     background-color: ${props => props.color || '#61942e'};
+//     color: #c92c2c;
+//     &:hover {
+//       background-color: transparent;
+//     }
+//   `}
+//
+//
+//   ${props => props.variant === 'outline' && css<SupperButtonProps>`
+//     color: ${props => props.color || '#61942e'};
+//     border: 2px solid ${props => props.color || '#61942e'};
+//     background-color: transparent;
+//     &:hover {
+//       background-color: chartreuse;
+//     }
+//   `}
+//   ${props => props.active && css<SupperButtonProps> `
+//     &:hover {
+//       box-shadow: 5px 5px 5px 13px rgba(178, 94, 94, 0.25);
+//     }
+//   `}
+//
+//   ${props => props.active && css<SupperButtonProps> `
+//     &:hover {
+//       box-shadow: 5px 5px 5px 13px rgba(178, 94, 94, 0.25);
+//     }
+//   `}
+// `
 
 
 
